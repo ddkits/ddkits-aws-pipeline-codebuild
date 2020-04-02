e4f8882753241f6a9f980733cc7411243c0c0c10
Sam Elayyoub <m22386@live.com>
2020-04-02 14:05:32 -0700

    Dcoker file included and custom changes for Laravel DDKits

--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,19 @@ Route::get('/home', 'HomeController@index')->name('home');
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
